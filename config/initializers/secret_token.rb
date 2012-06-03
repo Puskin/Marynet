ac1f98a9f52e0dbb5c46ba85ffe65c0e44a5a0dc
Puskin <mbpuskin@gmail.com>
2012-06-03 16:58:29 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marynet::Application.config.secret_token = 'ea0624494d5c9efaab33501eb632963f215977bf4346ed8e5f70ee472dd5794b7fa94bd93b06844e07ebd5e68efdd6e8fa9de05a9d42c0f0e217be09a997e90f'
