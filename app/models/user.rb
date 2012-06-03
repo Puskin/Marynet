0876a3d42270516c02ec8532b4b19b453ede5bfe
Puskin <mbpuskin@gmail.com>
2012-06-03 18:14:10 +0200

    User model refinements

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
 	validates :email,	presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	validates :password, length: { minimum: 6 }
 
+	before_save { |user| user.email = email.downcase }
 	before_save :create_remember_token
 
 
